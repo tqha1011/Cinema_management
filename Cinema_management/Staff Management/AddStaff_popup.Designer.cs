01bafe97d2ad3aab6b15a8b268ef2beb7dab28e0
Tran Quang Ha <tranquangha2006@gmail.com>
2025-11-17 08:08:49 +0700

    fix: size of Form pop_up

--- Cinema_management/Staff Management/AddStaff_popup.Designer.cs
+++ Cinema_management/Staff Management/AddStaff_popup.Designer.cs
@@ -40,7 +40,7 @@
             this.panelHost.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelHost.Location = new System.Drawing.Point(10, 10);
             this.panelHost.Name = "panelHost";
-            this.panelHost.Size = new System.Drawing.Size(680, 763);
+            this.panelHost.Size = new System.Drawing.Size(686, 787);
             this.panelHost.StateCommon.Color1 = System.Drawing.Color.White;
             this.panelHost.StateCommon.Color2 = System.Drawing.Color.White;
             this.panelHost.TabIndex = 0;
@@ -52,7 +52,7 @@
             this.addStaff1.Location = new System.Drawing.Point(0, 0);
             this.addStaff1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.addStaff1.Name = "addStaff1";
-            this.addStaff1.Size = new System.Drawing.Size(680, 763);
+            this.addStaff1.Size = new System.Drawing.Size(686, 787);
             this.addStaff1.TabIndex = 0;
             this.addStaff1.Load += new System.EventHandler(this.addStaff1_Load_1);
             // 
@@ -60,7 +60,7 @@
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(700, 783);
+            this.ClientSize = new System.Drawing.Size(706, 807);
             this.CloseBox = false;
             this.ControlBox = false;
             this.Controls.Add(this.panelHost);
