6a8aa8ab6c9c27909c12a88b3b02ec3ff6f13806
Tran Quang Ha <tranquangha2006@gmail.com>
2025-11-18 10:52:14 +0700

    new: Event for Update Button

--- Cinema_management/Staff Management/AddStaff.cs
+++ Cinema_management/Staff Management/AddStaff.cs
@@ -13,10 +13,13 @@ namespace Cinema_management
     public partial class AddStaff : UserControl
     {
         private StaffLogic staffLogic;
-        public AddStaff()
+        private int currentStaffId = 0; // Luu ID nhan vien hien tai (0 neu them moi)
+      
+        public AddStaff(int id)
         {
             InitializeComponent();
             staffLogic = new StaffLogic();
+            currentStaffId = id; // id = 0 thì thêm mới, id > 0 thì cập nhật
         }
 
         private void btnExitAddStaf_Click(object sender, EventArgs e)
@@ -34,6 +37,7 @@ namespace Cinema_management
 
         private void btnSaveStaff_Click(object sender, EventArgs e)
         {
+            bool success = false;
             Staff newStaff = new Staff();
             newStaff.Name = txbName.Text;
             newStaff.Gender = cbGender.Text;
@@ -45,14 +49,28 @@ namespace Cinema_management
             newStaff.AccountUsername = txbAccount.Text;
             newStaff.AccountPassword = txbPassword.Text;
             string passwordConfirm = txbPasswordConfirm.Text; // Xac nhan mat khau nhap lai
-            if (staffLogic.AddNewStaff(newStaff, passwordConfirm))
+            if (currentStaffId == 0)
             {
-                MessageBox.Show("Add successfully");
-                // tat form pop-up sau khi them thanh cong
+                // Them moi nhan vien
+                success = staffLogic.AddNewStaff(newStaff, passwordConfirm);
+            }
+            else
+            {
+                // Cap nhat nhan vien
+                newStaff.Id = currentStaffId;
+                success = staffLogic.UpdateStaff(newStaff, passwordConfirm, currentStaffId);
+            }
+            if(success)
+            {
+                MessageBox.Show("Lưu thông tin nhân viên thành công!", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 if (this.ParentForm != null)
                 {
                     this.ParentForm.Close();
                 }
+            }
+            else
+            {
+                MessageBox.Show("Lưu thông tin nhân viên thất bại. Vui lòng kiểm tra lại dữ liệu nhập.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -63,7 +81,28 @@ namespace Cinema_management
 
         private void AddStaff_Load(object sender, EventArgs e)
         {
+            if (currentStaffId > 0)
+            {
+                LoadStaffForUpdate();
+            }
+        }
 
+        private void LoadStaffForUpdate()
+        {
+            Staff existingStaff = staffLogic.GetStaffById(currentStaffId);
+            if (existingStaff != null)
+            {
+                txbName.Text = existingStaff.Name;
+                cbGender.Text = existingStaff.Gender;
+                cboPosition.Text = existingStaff.Position;
+                txbEmail.Text = existingStaff.Email;
+                txbPhoneNum.Text = existingStaff.PhoneNumber;
+                datePickerBirth.Value = existingStaff.BirthDate;
+                datePickerHire.Value = existingStaff.HireDate;
+                txbAccount.Text = existingStaff.AccountUsername;
+                txbPassword.Text = existingStaff.AccountPassword;
+                txbPasswordConfirm.Text = existingStaff.AccountPassword;
+            }
         }
     }
 }
