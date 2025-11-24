eba074f50ffd82d30f5757660971b5ccd102f82e
Tran Quang Ha <tranquangha2006@gmail.com>
2025-11-24 21:48:31 +0700

    Fix: size Messagebox Custom

--- Cinema_management/MessageboxCustom/Warning.Designer.cs
+++ Cinema_management/MessageboxCustom/Warning.Designer.cs
@@ -28,78 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.kryptonTableLayoutPanel1 = new Krypton.Toolkit.KryptonTableLayoutPanel();
             this.ptbWarning = new System.Windows.Forms.PictureBox();
+            this.lblmsg = new Krypton.Toolkit.KryptonWrapLabel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.btnOk = new Krypton.Toolkit.KryptonButton();
             this.btnCancel = new Krypton.Toolkit.KryptonButton();
-            this.lblmsg = new Krypton.Toolkit.KryptonWrapLabel();
+            this.kryptonTableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptbWarning)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // kryptonTableLayoutPanel1
+            // 
+            this.kryptonTableLayoutPanel1.AutoSize = true;
+            this.kryptonTableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kryptonTableLayoutPanel1.ColumnCount = 2;
+            this.kryptonTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.kryptonTableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.kryptonTableLayoutPanel1.Controls.Add(this.ptbWarning, 0, 0);
+            this.kryptonTableLayoutPanel1.Controls.Add(this.lblmsg, 1, 0);
+            this.kryptonTableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.kryptonTableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonTableLayoutPanel1.Location = new System.Drawing.Point(20, 20);
+            this.kryptonTableLayoutPanel1.Name = "kryptonTableLayoutPanel1";
+            this.kryptonTableLayoutPanel1.RowCount = 2;
+            this.kryptonTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.kryptonTableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.kryptonTableLayoutPanel1.Size = new System.Drawing.Size(314, 102);
+            this.kryptonTableLayoutPanel1.StateCommon.Color1 = System.Drawing.Color.White;
+            this.kryptonTableLayoutPanel1.TabIndex = 0;
             // 
             // ptbWarning
             // 
-            this.ptbWarning.Location = new System.Drawing.Point(15, 35);
+            this.ptbWarning.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.ptbWarning.BackColor = System.Drawing.Color.Transparent;
+            this.ptbWarning.Location = new System.Drawing.Point(3, 3);
             this.ptbWarning.Name = "ptbWarning";
-            this.ptbWarning.Size = new System.Drawing.Size(58, 40);
+            this.ptbWarning.Size = new System.Drawing.Size(34, 45);
             this.ptbWarning.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.ptbWarning.TabIndex = 0;
             this.ptbWarning.TabStop = false;
             // 
+            // lblmsg
+            // 
+            this.lblmsg.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblmsg.LabelStyle = Krypton.Toolkit.LabelStyle.AlternateControl;
+            this.lblmsg.Location = new System.Drawing.Point(43, 15);
+            this.lblmsg.MaximumSize = new System.Drawing.Size(320, 0);
+            this.lblmsg.Name = "lblmsg";
+            this.lblmsg.Size = new System.Drawing.Size(139, 20);
+            this.lblmsg.Text = "kryptonWrapLabel1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kryptonTableLayoutPanel1.SetColumnSpan(this.flowLayoutPanel1, 2);
+            this.flowLayoutPanel1.Controls.Add(this.btnOk);
+            this.flowLayoutPanel1.Controls.Add(this.btnCancel);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(57, 71);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 20, 3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(199, 28);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(55, 107);
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(3, 3);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(3, 3, 10, 3);
             this.btnOk.Name = "btnOk";
             this.btnOk.OverrideDefault.Back.Color1 = System.Drawing.Color.DarkOrange;
             this.btnOk.OverrideDefault.Back.Color2 = System.Drawing.Color.DarkOrange;
+            this.btnOk.OverrideDefault.Border.Color1 = System.Drawing.Color.Black;
+            this.btnOk.OverrideDefault.Border.Rounding = 9F;
             this.btnOk.Size = new System.Drawing.Size(90, 25);
             this.btnOk.StateCommon.Back.Color1 = System.Drawing.Color.DarkOrange;
             this.btnOk.StateCommon.Back.Color2 = System.Drawing.Color.DarkOrange;
-            this.btnOk.StateCommon.Border.Rounding = 8F;
+            this.btnOk.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnOk.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnOk.StateCommon.Border.Rounding = 9F;
             this.btnOk.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
-            this.btnOk.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOk.TabIndex = 1;
+            this.btnOk.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.StatePressed.Content.Padding = new System.Windows.Forms.Padding(-1, 2, -1, -1);
+            this.btnOk.TabIndex = 0;
             this.btnOk.Values.DropDownArrowColor = System.Drawing.Color.Empty;
             this.btnOk.Values.Text = "Đồng ý";
-            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(182, 107);
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(106, 3);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.OverrideDefault.Back.Color1 = System.Drawing.Color.SlateGray;
-            this.btnCancel.OverrideDefault.Back.Color2 = System.Drawing.Color.SlateGray;
+            this.btnCancel.OverrideDefault.Back.Color1 = System.Drawing.Color.DarkOrange;
+            this.btnCancel.OverrideDefault.Back.Color2 = System.Drawing.Color.DarkOrange;
+            this.btnCancel.OverrideDefault.Border.Color1 = System.Drawing.Color.Black;
+            this.btnCancel.OverrideDefault.Border.Rounding = 9F;
             this.btnCancel.Size = new System.Drawing.Size(90, 25);
-            this.btnCancel.StateCommon.Back.Color1 = System.Drawing.Color.SlateGray;
-            this.btnCancel.StateCommon.Back.Color2 = System.Drawing.Color.SlateGray;
-            this.btnCancel.StateCommon.Border.Rounding = 8F;
+            this.btnCancel.StateCommon.Back.Color1 = System.Drawing.Color.DarkOrange;
+            this.btnCancel.StateCommon.Back.Color2 = System.Drawing.Color.DarkOrange;
+            this.btnCancel.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnCancel.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnCancel.StateCommon.Border.Rounding = 9F;
             this.btnCancel.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
-            this.btnCancel.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.TabIndex = 2;
+            this.btnCancel.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.StatePressed.Content.Padding = new System.Windows.Forms.Padding(-1, 2, -1, -1);
+            this.btnCancel.TabIndex = 1;
             this.btnCancel.Values.DropDownArrowColor = System.Drawing.Color.Empty;
             this.btnCancel.Values.Text = "Không";
-            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
-            // 
-            // lblmsg
-            // 
-            this.lblmsg.AutoSize = false;
-            this.lblmsg.LabelStyle = Krypton.Toolkit.LabelStyle.AlternateControl;
-            this.lblmsg.Location = new System.Drawing.Point(79, 35);
-            this.lblmsg.Name = "lblmsg";
-            this.lblmsg.Size = new System.Drawing.Size(282, 40);
-            this.lblmsg.Text = "kryptonWrapLabel1";
             // 
             // Warning
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(350, 156);
-            this.Controls.Add(this.lblmsg);
-            this.Controls.Add(this.btnCancel);
-            this.Controls.Add(this.btnOk);
-            this.Controls.Add(this.ptbWarning);
+            this.ClientSize = new System.Drawing.Size(354, 142);
+            this.Controls.Add(this.kryptonTableLayoutPanel1);
             this.Location = new System.Drawing.Point(0, 0);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Warning";
+            this.Padding = new System.Windows.Forms.Padding(20);
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.StateCommon.Back.Color1 = System.Drawing.Color.White;
@@ -113,16 +166,22 @@
             this.StateCommon.Header.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Text = "Cảnh báo";
             this.Load += new System.EventHandler(this.Warning_Load);
+            this.kryptonTableLayoutPanel1.ResumeLayout(false);
+            this.kryptonTableLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ptbWarning)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private Krypton.Toolkit.KryptonTableLayoutPanel kryptonTableLayoutPanel1;
         private System.Windows.Forms.PictureBox ptbWarning;
+        private Krypton.Toolkit.KryptonWrapLabel lblmsg;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private Krypton.Toolkit.KryptonButton btnOk;
         private Krypton.Toolkit.KryptonButton btnCancel;
-        private Krypton.Toolkit.KryptonWrapLabel lblmsg;
     }
 }
