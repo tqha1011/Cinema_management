a91ea844be533cd2006a38d578610a40c0bb4798
Thao-Tien <lilianjung2036@gmail.com>
2025-12-02 14:38:29 +0700

    Update Booking tickets

--- Cinema_management/Ticket Booking/UCMovieCard.cs
+++ Cinema_management/Ticket Booking/UCMovieCard.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,91 @@ namespace Cinema_management.Ticket_Booking
 {
     public partial class UCMovieCard : UserControl
     {
+        //Sự kiện để báo ra bên ngoài "card đã được click"
+        public event EventHandler OnSelect;
+
+        public int MaPhim {  get; set; } //lưu ID phim ẩn
+
+        // Đường dẫn thư mục chứa ảnh (Ngang hàng với file .csproj và Form1.cs)
+        // Lưu ý: Dùng Path.GetFullPath để chuẩn hóa đường dẫn
+        private string _posterFolderPath = Path.GetFullPath(Path.Combine(Application.StartupPath, @"..\..\Posters"));
+
         public UCMovieCard()
         {
             InitializeComponent();
+            //gán sự kiện click cho cả poster và tên phim
+            this.Click += UCMovieCard_Click;
+            picPoster.Click += UCMovieCard_Click;
+            lbTitle.Click += UCMovieCard_Click;
+
+            // Tạo thư mục nếu chưa có (để tránh lỗi)
+            if (!Directory.Exists(_posterFolderPath))
+            {
+                Directory.CreateDirectory(_posterFolderPath);
+            }
+        }
+
+        public void SetData(int maPhim, string tenPhim, string pathAnh)
+        {
+            this.MaPhim = maPhim;
+            lbTitle.Text = tenPhim;
+
+            //xử lý đường dẫn ảnh
+            //try
+            //{
+            //    if (!string.IsNullOrEmpty(pathAnh))
+            //    {
+            //        // Nếu đường dẫn là tương đối, ghép nó với thư mục chạy của phần mềm
+            //        // Ví dụ CSDL lưu "images/abc.jpg" -> ghép thành "C:/.../bin/Debug/images/abc.jpg"
+            //        string projectPath = Application.StartupPath;
+            //        string fullPath = System.IO.Path.Combine(projectPath, pathAnh);
+
+            //        if (System.IO.File.Exists(fullPath))
+            //        {
+            //            picPoster.Image = Image.FromFile(fullPath);
+            //            picPoster.SizeMode = PictureBoxSizeMode.StretchImage; // Co giãn ảnh cho vừa khung
+            //        }
+            //        else
+            //        {
+            //            // Nếu không tìm thấy file, có thể load ảnh mặc định hoặc để trống
+            //            // picPoster.Image = Properties.Resources.NoImage; 
+            //            picPoster.Image = null;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        picPoster.Image = null;
+            //    }
+            //}
+            //catch
+            //{
+            //    // Nếu file ảnh bị lỗi định dạng, bỏ qua, không crash app
+            //    picPoster.Image = null;
+            //}
+
+            // --- LOAD ẢNH ---
+            string fileName = pathAnh;
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                string fullPath = Path.Combine(_posterFolderPath, fileName);
+                if (File.Exists(fullPath))
+                {
+                    picPoster.Image = Image.FromFile(fullPath);
+                    picPoster.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+            }
+        }
+        
+
+        private void UCMovieCard_Click(object sender, EventArgs e)
+        {
+            //kich hoạt sự kiện để UC cha biết
+            OnSelect?.Invoke(this, e);
+        }
+
+        private void UCMovieCard_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Cinema_management/Ticket Booking/UCPhimDangChieu.Designer.cs
+++ Cinema_management/Ticket Booking/UCPhimDangChieu.Designer.cs
@@ -32,20 +32,21 @@
             this.txtSearchMovie = new Krypton.Toolkit.KryptonTextBox();
             this.dtpSearchDate = new Krypton.Toolkit.KryptonDateTimePicker();
             this.panelMM = new Krypton.Toolkit.KryptonPanel();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flowPanelMovies = new System.Windows.Forms.FlowLayoutPanel();
             this.ucMovieCard1 = new Cinema_management.Ticket_Booking.UCMovieCard();
             ((System.ComponentModel.ISupportInitialize)(this.panelMM)).BeginInit();
             this.panelMM.SuspendLayout();
-            this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowPanelMovies.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblMovieManagement
             // 
-            this.lblMovieManagement.Location = new System.Drawing.Point(68, 54);
+            this.lblMovieManagement.Location = new System.Drawing.Point(91, 68);
+            this.lblMovieManagement.Margin = new System.Windows.Forms.Padding(4);
             this.lblMovieManagement.Name = "lblMovieManagement";
-            this.lblMovieManagement.Size = new System.Drawing.Size(317, 42);
+            this.lblMovieManagement.Size = new System.Drawing.Size(421, 55);
             this.lblMovieManagement.StateCommon.ShortText.Color1 = System.Drawing.Color.Black;
             this.lblMovieManagement.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft YaHei UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMovieManagement.TabIndex = 1;
@@ -56,9 +57,10 @@
             this.txtSearchMovie.CueHint.Color1 = System.Drawing.Color.Gray;
             this.txtSearchMovie.CueHint.CueHintText = "Search movie...";
             this.txtSearchMovie.CueHint.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSearchMovie.Location = new System.Drawing.Point(86, 143);
+            this.txtSearchMovie.Location = new System.Drawing.Point(115, 179);
+            this.txtSearchMovie.Margin = new System.Windows.Forms.Padding(4);
             this.txtSearchMovie.Name = "txtSearchMovie";
-            this.txtSearchMovie.Size = new System.Drawing.Size(300, 36);
+            this.txtSearchMovie.Size = new System.Drawing.Size(400, 44);
             this.txtSearchMovie.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(227)))));
             this.txtSearchMovie.StateCommon.Border.Rounding = 10F;
             this.txtSearchMovie.StateCommon.Content.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -70,13 +72,14 @@
             this.dtpSearchDate.CalendarTodayDate = new System.DateTime(2025, 10, 28, 0, 0, 0, 0);
             this.dtpSearchDate.CustomFormat = "dd/MM/yyyy";
             this.dtpSearchDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dtpSearchDate.Location = new System.Drawing.Point(452, 143);
+            this.dtpSearchDate.Location = new System.Drawing.Point(603, 179);
+            this.dtpSearchDate.Margin = new System.Windows.Forms.Padding(4);
             this.dtpSearchDate.Name = "dtpSearchDate";
-            this.dtpSearchDate.ShowCheckBox = true;
-            this.dtpSearchDate.Size = new System.Drawing.Size(191, 36);
+            this.dtpSearchDate.Size = new System.Drawing.Size(255, 43);
             this.dtpSearchDate.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(227)))));
             this.dtpSearchDate.StateCommon.Border.Rounding = 10F;
             this.dtpSearchDate.TabIndex = 6;
+            this.dtpSearchDate.ValueChanged += new System.EventHandler(this.dtpSearchDate_ValueChanged);
             // 
             // panelMM
             // 
@@ -86,57 +89,63 @@
             this.panelMM.Controls.Add(this.dtpSearchDate);
             this.panelMM.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelMM.Location = new System.Drawing.Point(0, 0);
+            this.panelMM.Margin = new System.Windows.Forms.Padding(4);
             this.panelMM.Name = "panelMM";
-            this.panelMM.Padding = new System.Windows.Forms.Padding(20);
-            this.panelMM.Size = new System.Drawing.Size(900, 179);
+            this.panelMM.Padding = new System.Windows.Forms.Padding(27, 25, 27, 25);
+            this.panelMM.Size = new System.Drawing.Size(1200, 224);
             this.panelMM.StateCommon.Color1 = System.Drawing.Color.Transparent;
             this.panelMM.TabIndex = 9;
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.AutoScroll = true;
-            this.flowLayoutPanel1.Controls.Add(this.ucMovieCard1);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 179);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(50, 40, 40, 40);
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(900, 431);
-            this.flowLayoutPanel1.TabIndex = 10;
+            this.panelMM.Paint += new System.Windows.Forms.PaintEventHandler(this.panelMM_Paint);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Cinema_management.Properties.Resources.lucide__search;
-            this.pictureBox1.Location = new System.Drawing.Point(56, 143);
+            this.pictureBox1.Location = new System.Drawing.Point(75, 179);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(24, 36);
+            this.pictureBox1.Size = new System.Drawing.Size(32, 45);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
+            // flowPanelMovies
+            // 
+            this.flowPanelMovies.AutoScroll = true;
+            this.flowPanelMovies.Controls.Add(this.ucMovieCard1);
+            this.flowPanelMovies.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowPanelMovies.Location = new System.Drawing.Point(0, 224);
+            this.flowPanelMovies.Margin = new System.Windows.Forms.Padding(4);
+            this.flowPanelMovies.Name = "flowPanelMovies";
+            this.flowPanelMovies.Padding = new System.Windows.Forms.Padding(67, 50, 53, 50);
+            this.flowPanelMovies.Size = new System.Drawing.Size(1200, 538);
+            this.flowPanelMovies.TabIndex = 10;
+            // 
             // ucMovieCard1
             // 
             this.ucMovieCard1.BackColor = System.Drawing.Color.White;
-            this.ucMovieCard1.Location = new System.Drawing.Point(60, 50);
-            this.ucMovieCard1.Margin = new System.Windows.Forms.Padding(10);
+            this.ucMovieCard1.Location = new System.Drawing.Point(80, 62);
+            this.ucMovieCard1.MaPhim = 0;
+            this.ucMovieCard1.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
             this.ucMovieCard1.Name = "ucMovieCard1";
-            this.ucMovieCard1.Padding = new System.Windows.Forms.Padding(10);
-            this.ucMovieCard1.Size = new System.Drawing.Size(230, 340);
+            this.ucMovieCard1.Padding = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.ucMovieCard1.Size = new System.Drawing.Size(307, 425);
             this.ucMovieCard1.TabIndex = 0;
             // 
             // UCPhimDangChieu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.flowPanelMovies);
             this.Controls.Add(this.panelMM);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "UCPhimDangChieu";
-            this.Size = new System.Drawing.Size(900, 610);
+            this.Size = new System.Drawing.Size(1200, 762);
             ((System.ComponentModel.ISupportInitialize)(this.panelMM)).EndInit();
             this.panelMM.ResumeLayout(false);
             this.panelMM.PerformLayout();
-            this.flowLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowPanelMovies.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -148,7 +157,7 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private Krypton.Toolkit.KryptonDateTimePicker dtpSearchDate;
         private Krypton.Toolkit.KryptonPanel panelMM;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelMovies;
         private UCMovieCard ucMovieCard1;
     }
 }
