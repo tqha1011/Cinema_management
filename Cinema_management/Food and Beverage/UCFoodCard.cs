3e44377302f1edeca21ddf17380cffa1770127d6
anhvansan <anhvantran.0910@gmail.com>
2025-12-07 01:18:35 +0700

    uc quan ly do an

---
+++ Cinema_management/Food and Beverage/UCFoodCard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Krypton.Toolkit;
+
+namespace Cinema_management
+{
+    public partial class UCFoodCard : UserControl
+    {
+        public UCFoodCard()
+        {
+            InitializeComponent();
+        }
+
+        private void lbTenMon_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
