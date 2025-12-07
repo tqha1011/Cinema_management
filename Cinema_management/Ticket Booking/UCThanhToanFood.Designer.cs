14aaef3c84500ed83ba96e99128ef153924a2496
anhvansan <anhvantran.0910@gmail.com>
2025-12-07 21:25:21 +0700

    form chon do an cho ban ve

---
+++ Cinema_management/Ticket Booking/UCThanhToanFood.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Cinema_management.Ticket_Booking
+{
+    partial class UCThanhToanFood
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTenMon = new Krypton.Toolkit.KryptonLabel();
+            this.lbGia = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonButton2 = new Krypton.Toolkit.KryptonButton();
+            this.txtSoLuong = new Krypton.Toolkit.KryptonTextBox();
+            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            this.btnDelete = new Krypton.Toolkit.KryptonButton();
+            this.kryptonPictureBox1 = new Krypton.Toolkit.KryptonPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbTenMon
+            // 
+            this.lbTenMon.Location = new System.Drawing.Point(168, 13);
+            this.lbTenMon.Name = "lbTenMon";
+            this.lbTenMon.Size = new System.Drawing.Size(189, 44);
+            this.lbTenMon.StateCommon.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.lbTenMon.StateCommon.ShortText.Font = new System.Drawing.Font("Nunito", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenMon.TabIndex = 1;
+            this.lbTenMon.Values.Text = "Bắp Caramel";
+            // 
+            // lbGia
+            // 
+            this.lbGia.Location = new System.Drawing.Point(173, 63);
+            this.lbGia.Name = "lbGia";
+            this.lbGia.Size = new System.Drawing.Size(131, 38);
+            this.lbGia.StateCommon.ShortText.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.lbGia.StateCommon.ShortText.Font = new System.Drawing.Font("Nunito", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGia.TabIndex = 1;
+            this.lbGia.Values.Text = "100.000 đ";
+            // 
+            // kryptonButton2
+            // 
+            this.kryptonButton2.Location = new System.Drawing.Point(170, 104);
+            this.kryptonButton2.Name = "kryptonButton2";
+            this.kryptonButton2.OverrideDefault.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.OverrideDefault.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.OverrideFocus.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.OverrideFocus.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.OverrideFocus.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.OverrideFocus.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.kryptonButton2.Size = new System.Drawing.Size(50, 44);
+            this.kryptonButton2.StateNormal.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateNormal.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateNormal.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StatePressed.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StatePressed.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StatePressed.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.kryptonButton2.StateTracking.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateTracking.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton2.StateTracking.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.kryptonButton2.TabIndex = 9;
+            this.kryptonButton2.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton2.Values.Image = global::Cinema_management.Properties.Resources.fluent__subtract_circle_12_regular;
+            this.kryptonButton2.Values.ImageStates.ImageCheckedNormal = null;
+            this.kryptonButton2.Values.ImageStates.ImageCheckedPressed = null;
+            this.kryptonButton2.Values.ImageStates.ImageCheckedTracking = null;
+            this.kryptonButton2.Values.ImageStates.ImageNormal = global::Cinema_management.Properties.Resources.fluent__subtract_circle_24_regular;
+            this.kryptonButton2.Values.ImageStates.ImagePressed = global::Cinema_management.Properties.Resources.fluent__subtract_circle_16_filled;
+            this.kryptonButton2.Values.ImageStates.ImageTracking = global::Cinema_management.Properties.Resources.fluent__subtract_circle_16_filled;
+            this.kryptonButton2.Values.Text = "";
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(223, 107);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(67, 38);
+            this.txtSoLuong.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(219)))), ((int)(((byte)(219)))), ((int)(((byte)(219)))));
+            this.txtSoLuong.StateCommon.Border.Color1 = System.Drawing.Color.Transparent;
+            this.txtSoLuong.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.txtSoLuong.StateCommon.Border.Width = 2;
+            this.txtSoLuong.StateCommon.Content.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtSoLuong.StateCommon.Content.Font = new System.Drawing.Font("Nunito", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoLuong.TabIndex = 8;
+            this.txtSoLuong.Text = "0";
+            this.txtSoLuong.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(293, 104);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.OverrideDefault.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.OverrideDefault.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.OverrideFocus.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.OverrideFocus.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.OverrideFocus.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.OverrideFocus.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.kryptonButton1.Size = new System.Drawing.Size(50, 44);
+            this.kryptonButton1.StateNormal.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateNormal.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateNormal.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StatePressed.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StatePressed.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StatePressed.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.kryptonButton1.StateTracking.Back.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateTracking.Back.Color2 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateTracking.Border.Color1 = System.Drawing.Color.Transparent;
+            this.kryptonButton1.StateTracking.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.kryptonButton1.TabIndex = 7;
+            this.kryptonButton1.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton1.Values.Image = global::Cinema_management.Properties.Resources.gg__add;
+            this.kryptonButton1.Values.ImageStates.ImageCheckedNormal = null;
+            this.kryptonButton1.Values.ImageStates.ImageCheckedPressed = null;
+            this.kryptonButton1.Values.ImageStates.ImageCheckedTracking = null;
+            this.kryptonButton1.Values.ImageStates.ImageNormal = global::Cinema_management.Properties.Resources.gg__add;
+            this.kryptonButton1.Values.ImageStates.ImagePressed = global::Cinema_management.Properties.Resources.carbon__add_filled;
+            this.kryptonButton1.Values.ImageStates.ImageTracking = global::Cinema_management.Properties.Resources.carbon__add_filled;
+            this.kryptonButton1.Values.Text = "";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(416, 13);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.OverrideDefault.Back.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.OverrideDefault.Back.Color2 = System.Drawing.Color.Transparent;
+            this.btnDelete.OverrideFocus.Back.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.OverrideFocus.Back.Color2 = System.Drawing.Color.Transparent;
+            this.btnDelete.OverrideFocus.Border.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.OverrideFocus.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.btnDelete.Size = new System.Drawing.Size(50, 44);
+            this.btnDelete.StateNormal.Back.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.StateNormal.Back.Color2 = System.Drawing.Color.Transparent;
+            this.btnDelete.StateNormal.Border.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.StatePressed.Back.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.StatePressed.Back.Color2 = System.Drawing.Color.Transparent;
+            this.btnDelete.StatePressed.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.btnDelete.StateTracking.Back.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.StateTracking.Back.Color2 = System.Drawing.Color.Transparent;
+            this.btnDelete.StateTracking.Border.Color1 = System.Drawing.Color.Transparent;
+            this.btnDelete.StateTracking.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.None;
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnDelete.Values.ImageStates.ImageCheckedNormal = null;
+            this.btnDelete.Values.ImageStates.ImageCheckedPressed = null;
+            this.btnDelete.Values.ImageStates.ImageCheckedTracking = null;
+            this.btnDelete.Values.ImageStates.ImageNormal = global::Cinema_management.Properties.Resources.streamline_ultimate__bin_1;
+            this.btnDelete.Values.ImageStates.ImagePressed = global::Cinema_management.Properties.Resources.streamline_ultimate__bin_1_bold;
+            this.btnDelete.Values.ImageStates.ImageTracking = global::Cinema_management.Properties.Resources.streamline_ultimate__bin_1_bold;
+            this.btnDelete.Values.Text = "";
+            // 
+            // kryptonPictureBox1
+            // 
+            this.kryptonPictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.kryptonPictureBox1.Name = "kryptonPictureBox1";
+            this.kryptonPictureBox1.Size = new System.Drawing.Size(141, 132);
+            this.kryptonPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.kryptonPictureBox1.TabIndex = 0;
+            this.kryptonPictureBox1.TabStop = false;
+            // 
+            // UCThanhToanFood
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.kryptonButton2);
+            this.Controls.Add(this.txtSoLuong);
+            this.Controls.Add(this.kryptonButton1);
+            this.Controls.Add(this.lbGia);
+            this.Controls.Add(this.lbTenMon);
+            this.Controls.Add(this.kryptonPictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MinimumSize = new System.Drawing.Size(480, 159);
+            this.Name = "UCThanhToanFood";
+            this.Size = new System.Drawing.Size(480, 159);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Krypton.Toolkit.KryptonPictureBox kryptonPictureBox1;
+        private Krypton.Toolkit.KryptonLabel lbTenMon;
+        private Krypton.Toolkit.KryptonLabel lbGia;
+        private Krypton.Toolkit.KryptonButton kryptonButton2;
+        private Krypton.Toolkit.KryptonTextBox txtSoLuong;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+        private Krypton.Toolkit.KryptonButton btnDelete;
+    }
+}
