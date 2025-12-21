0f51ba2c23d65da9f41d3f2930557a5f60bf3fc9
anhvansan <anhvantran.0910@gmail.com>
2025-12-22 01:34:15 +0700

    edit

--- Cinema_management/Program.cs
+++ Cinema_management/Program.cs
@@ -16,8 +16,8 @@ namespace Cinema_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new FormLogin());
-            Application.Run(new FormMain());
+            Application.Run(new FormLogin());
+            //Application.Run(new FormMain());
         }
     }
 }
