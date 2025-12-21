3e55048c565f0effc839b35e4fc77dc369607ce7
anhvansan <anhvantran.0910@gmail.com>
2025-12-21 18:16:53 +0700

    edit: uc order food UI

--- Cinema_management/Program.cs
+++ Cinema_management/Program.cs
@@ -16,7 +16,8 @@ namespace Cinema_management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
+            //Application.Run(new FormLogin());
+            Application.Run(new FormMain());
         }
     }
 }
